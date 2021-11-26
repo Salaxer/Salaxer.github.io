ceca72e57bb4ff78a373d97f5c6e213b3b8c0f53
Salaxer <hector_salazar12@hotmail.com>
2021-11-26 17:02:26 -0600

    platzi badges

---
+++ Platzi Badges/precache-manifest.50dda2861326d3b5bce4bb12879bc85e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2060e1ba0a1d8b48b1d5",
+    "url": "/static/css/main.094ef308.chunk.css"
+  },
+  {
+    "revision": "2060e1ba0a1d8b48b1d5",
+    "url": "/static/js/main.2060e1ba.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1e3be8da72878d0f5b58",
+    "url": "/static/css/2.1bc2f83c.chunk.css"
+  },
+  {
+    "revision": "1e3be8da72878d0f5b58",
+    "url": "/static/js/2.1e3be8da.chunk.js"
+  },
+  {
+    "revision": "ea20c0361ef9e0508b5439b699b5de58",
+    "url": "/static/media/platzi-conf-logo.ea20c036.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "f3eb5f457a9a65e70d5a0e998da8ba82",
+    "url": "/static/media/start.f3eb5f45.svg"
+  },
+  {
+    "revision": "1c2f188cb3d432d81c275eefdd8b88e6",
+    "url": "/static/media/astrounautas.1c2f188c.svg"
+  },
+  {
+    "revision": "50f98d91e8e3fbea6127a8d6784169ee",
+    "url": "/static/media/logo.50f98d91.svg"
+  },
+  {
+    "revision": "e7fbddae14de637d5f8747237594f78c",
+    "url": "/index.html"
+  }
+];
